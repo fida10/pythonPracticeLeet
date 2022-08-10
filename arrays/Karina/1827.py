25aa34bd5086e383de4b4e60c3a354775dcfd290
TheSands21 <prof5864.com>
2022-08-09 21:58:33 -0400

    #1827. Minimum Operations to Make the Array Increasing

---
+++ arrays/Karina/1827.py
@@ -0,0 +1,22 @@
+#1827. Minimum Operations to Make the Array Increasing
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        aLen = len(nums)
+        counter = 0;
+        for i in range(0, (aLen - 1)):
+            if nums[i] >= nums[i + 1]:
+                counter += (abs(nums[i] - nums[i + 1])) + 1
+                nums[i + 1] = nums[i + 1] + (abs(nums[i] - nums[i + 1])) + 1
+
+        return (counter)
+
+#Troubles shooting
+#nums = [1,5,2,4,1]
+#aLen = len(nums)
+#counter = 0;
+#for i in range(0, (aLen - 1)):
+#    if nums[i] >= nums[i + 1]:
+#        counter += (abs(nums[i] - nums[i + 1])) + 1
+#        nums[i + 1] = nums[i + 1] + (abs(nums[i] - nums[i + 1])) + 1
+#print(nums)
+#print(counter)
