71eac52f41467d7660cf750f4ebc767ef65ef5a2
TheSands21 <prof5864.com>
2022-08-20 00:32:01 -0400

    #1304. Find N Unique Integers Sum up to Zero

---
+++ arrays/Karina/1304.py
@@ -0,0 +1,14 @@
+#1304. Find N Unique Integers Sum up to Zero
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        nums = []
+        while (len(nums)) < n - 1:
+            val = len(nums)
+            nums.append(val + 1)
+            nums.append((val + 1) * (-1))
+
+        if n % 2 != 0:
+            nums.append(0)
+            # (note two lines were skipped  to make a global return)
+        # (note two lines were skipped  to make a global return)
+        return (nums)
